232249a6d34702f0ec261aa19a94d84a1182255e
luxian <luxianaiolos@gmail.com>
2022-05-28 15:26:00 -0700

    Feedback Stats component

--- feedback-app/src/App.jsx
+++ feedback-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import FeedbackList from "./components/FeedbackList"
+import FeedbackStats from './components/FeedbackStats'
 import Header from "./components/Header"
 import FeedbackData from './data/feedbackData'
 
@@ -7,10 +8,20 @@ import FeedbackData from './data/feedbackData'
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
 
+  const deleteFeedback = (id) => {
+    if(window.confirm('are you sure you wwant to delete?')) {
+      setFeedback(feedback.filter((item)=> item.id !== id))
+    }
+  }
+
   return (
     <>
       <Header />
-      <FeedbackList feedback={feedback} />
+      <FeedbackStats feedback={feedback}/>
+      <FeedbackList 
+        feedback={feedback} 
+        handleDelete={deleteFeedback}
+      />
     </>
     
 
--- feedback-app/src/components/shared/Card.jsx
+++ feedback-app/src/components/shared/Card.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Card() {
+function Card({children, reverse}) {
   return (
-    <div>
-      card
+    <div className={`card ${reverse && 'reverse'}`}>
+      {children}
     </div>
   )
+}
+
+Card.defaultProps = {
+  reverse: false,
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  reverse: PropTypes.bool,
 }
 
 export default Card
