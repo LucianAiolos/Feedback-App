b2d8a46e6d47e99d1f1e348a0a460a8e1f33ea03
luxian <luxianaiolos@gmail.com>
2022-06-02 01:07:11 -0700

    Finally Added  rating buttons 5-10

--- feedback-app/src/components/context/FeedbackContext.js
+++ feedback-app/src/components/context/FeedbackContext.js
@@ -7,26 +7,55 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'This item is from context',
+      text: 'This is Feedback item 1',
       rating: 10
+    },
+    {
+      id: 2,
+      text: 'This is Feedback item 2',
+      rating: 9
+    },
+    {
+      id: 3,
+      text: 'This is Feedback item 3',
+      rating: 8
     }
   ])
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  })
+
+  //Delete feedback
   const deleteFeedback = (id) => {
-      if(window.confirm('are you sure you wwant to delete?')) {
+      if(window.confirm('are you sure you want to delete?')) {
         setFeedback(feedback.filter((item)=> item.id !== id))
       }
     } 
   
+
+  //Add Feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
+  }
+
+
+  //Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true
+    })
   }
 
   return <FeedbackContext.Provider value={{
     feedback,
     deleteFeedback,
     addFeedback,
+    editFeedback,
+    feedbackEdit
   }}>
     {children}
   </FeedbackContext.Provider>
