7ac9a4b38257ca1e490f5c68abb9becc9baaf698
luxian <luxianaiolos@gmail.com>
2022-06-06 01:07:11 -0700

    Fixed JSON server/ added spinner

--- feedback-app/src/components/context/FeedbackContext.js
+++ feedback-app/src/components/context/FeedbackContext.js
@@ -1,31 +1,28 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This is Feedback item 1',
-      rating: 10
-    },
-    {
-      id: 2,
-      text: 'This is Feedback item 2',
-      rating: 9
-    },
-    {
-      id: 3,
-      text: 'This is Feedback item 3',
-      rating: 8
-    }
-  ])
-
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false
   })
+
+  useEffect(()=> {
+    fetchFeedback()
+  }, [])
+
+  // Fetch Feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(`http://localhost:5000/feedback?_sort=id&desc`)
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   //Delete feedback
   const deleteFeedback = (id) => {
@@ -60,6 +57,7 @@ export const FeedbackProvider = ({ children }) => {
   return <FeedbackContext.Provider value={{
     feedback,
     feedbackEdit,
+    isLoading,
     deleteFeedback,
     addFeedback,
     editFeedback,
